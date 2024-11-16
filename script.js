2be538fa941cf0699d8ec824f73980a538d78852
blit14466 <byamukamarap@gmail.com>
2024-11-16 12:15:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 document.getElementById('startGame').addEventListener('click', function() {
     alert('Game starting soon...');
 });
+document.addEventListener('DOMContentLoaded', function() {
+    var board = Chessboard('#board');
+    var startBtn = document.getElementById('startGameBtn');
+    
+    // Start the game when the button is clicked
+    startBtn.addEventListener('click', function() {
+        board.start();
+    });
+});
